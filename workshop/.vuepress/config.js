2e53632858d50dc4c3439b6453d3d844ada1d45c
Filipe Barroso <filipe.barroso@gmail.com>
2019-03-17 21:47:43 +0000

    Add Contribute URL

--- workshop/.vuepress/config.js
+++ workshop/.vuepress/config.js
@@ -2,6 +2,22 @@ module.exports = {
   title: "Flutter Hands On",
   description: "A hands on workshop to learn Flutter step by step",
   themeConfig: {
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'OldMetalmind/FlutterWorkshop',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    repoLabel: 'Contribute!',
+    // Optional options for generating "Edit this page" link
+    // if your docs are in a different repo from your main project:
+    //docsRepo: 'vuejs/vuepress',
+    // if your docs are not at the root of the repo:
+    docsDir: 'workshop',
+    // if your docs are in a specific branch (defaults to 'master'):
+    //docsBranch: 'master',
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Help us improve this page!',
     lastUpdated: 'Last Updated',
     sidebar: 'auto',
     nav: [
